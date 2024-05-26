ba1e112441a17f5ebebe3b941c1ce713afb7c699
moraguma <g170603@dac.unicamp.br>
2024-05-26 13:31:09 -0300

    Added README to PyPI

--- build/lib/build/lib/build/lib/build/lib/build/lib/torchbringer/__init__.py
+++ build/lib/build/lib/build/lib/build/lib/build/lib/torchbringer/__init__.py
@@ -4,6 +4,6 @@ TorchBringer
 A PyTorch library for deep reinforcement learning 
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = 'Moraguma'
 __credits__ = ''
--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_namespace_packages
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='torchbringer',
-    version='0.1.0',    
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='0.1.1',    
     description='A PyTorch library for deep reinforcement learning ',
     url='https://github.com/moraguma/TorchBringer',
     author='Moraguma',
